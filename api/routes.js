d888e554ad9fe725083c6c8f72afa9f1336bd9af
Elie Khoury <elie@el-khoury.net>
2017-08-23 20:54:21 +0200

    #8

--- api/routes.js
+++ api/routes.js
@@ -1,6 +1,6 @@
-var appRouter = function(app, passport, upload) {
+var appRouter = function(app, passport, upload, fs) {
     app.get(
-        "/images/:reference",
+        "/images/get/:reference",
         passport.authenticate("digest", { session: false }),
         function(req, res) {
             var reference = req.params.reference;
@@ -14,6 +14,21 @@ var appRouter = function(app, passport, upload) {
         upload.single("recipe_img"),
         function(req, res, next) {
             res.json(req.file);
+        }
+    );
+
+    app.post(
+        "/images/rm/:reference",
+        passport.authenticate("digest", { session: false }),
+        function(req, res) {
+            var fileUri = __dirname + "/store/" + req.params.reference;
+            fs.unlink(fileUri, function(err) {
+                if (err) {
+                    res.json("{deleted: false}");
+                } else {
+                    res.json("{deleted: true}");
+                }
+            });
         }
     );
 };
