d3303cfeeb7df2dacd3eeee190611faf1101d316
Elie Khoury <elie@el-khoury.net>
2017-08-23 07:13:58 +0200

    js

--- api/app.js
+++ api/app.js
@@ -5,11 +5,68 @@ var http = require("http");
 var util = require("util");
 var path = require("path");
 var app = express();
+var passport = require("passport");
+var DigestStrategy = require("passport-http").DigestStrategy;
+var multer = require("multer");
+const USERNAME = "elie";
+const PASSWORD = "pwd";
+var storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, "store/");
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.originalname.toLowerCase());
+    }
+});
 
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: true }));
+function fileFilter(req, file, cb) {
+    if (file.mimetype == null) {
+        cb(null, false);
+    } else if (
+        file.mimetype.indexOf("jpeg") ||
+        file.mimetype.indexOf("png") ||
+        file.mimetype.indexOf("gif")
+    ) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
 
-var routes = require("./routes.js")(app);
+var upload = multer({
+    storage: storage,
+    fileFilter: fileFilter,
+    limits: {
+        files: 1,
+        fileSize: 1000000,
+        fields: 2
+    }
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+passport.use(
+    new DigestStrategy({
+            qop: "auth"
+        },
+        function(username, done) {
+
+            if (username == USERNAME) {
+                return done(null, username, PASSWORD);
+            } else {
+                return done("Wrong credentials.");
+            }
+        },
+        function(params, done) {
+            // validate nonces as necessary
+            done(null, true);
+        }
+    )
+);
+
+var routes = require("./routes.js")(app, passport, upload);
 
 var server = app.listen(3000, function() {
     console.log("Listening on port %s...", server.address().port);
--- api/routes.js
+++ api/routes.js
@@ -1,47 +1,21 @@
-var multer = require("multer");
-//var upload = multer({ dest: "store/" });
-var storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, "store/");
-    },
-    filename: function(req, file, cb) {
-        cb(null, file.originalname.toLowerCase());
-    }
-});
+var appRouter = function(app, passport, upload) {
+    app.get(
+        "/images/:reference",
+        passport.authenticate("digest", { session: false }),
+        function(req, res) {
+            var reference = req.params.reference;
+            res.sendFile(__dirname + "/store/" + reference);
+        }
+    );
 
-function fileFilter(req, file, cb) {
-    if (file.mimetype.indexOf('jpeg') ||
-        file.mimetype.indexOf('png') ||
-        file.mimetype.indexOf('gif')) {
-        cb(null, true);
-    } else {
-        cb(null, false);
-    }
-}
-
-var upload = multer({
-    storage: storage,
-    fileFilter: fileFilter,
-    limits: {
-        files: 1,
-        fileSize: 1000000,
-        fields: 2
-    }
-});
-
-var appRouter = function(app) {
-    app.get("/images/:reference", function(req, res) {
-        var reference = req.params.reference;
-        res.sendFile(__dirname + "/store/" + reference);
-    });
-
-    app.post("/images/upload", upload.single("recipe_img"), function(
-        req,
-        res,
-        next
-    ) {
-        res.json(req.file);
-    });
+    app.post(
+        "/images/upload",
+        passport.authenticate("digest", { session: false }),
+        upload.single("recipe_img"),
+        function(req, res, next) {
+            res.json(req.file);
+        }
+    );
 };
 
 module.exports = appRouter;
