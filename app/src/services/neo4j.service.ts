abaaed89f2f8c23eb9c4ce47442044e45e5c6a44
Elie el Khoury <elie.el-khoury@t-systems.com>
2017-09-21 17:12:18 +0200

    using proper naming

--- app/src/services/neo4j.service.ts
+++ app/src/services/neo4j.service.ts
@@ -113,7 +113,7 @@ export class Neo4JService {
   }
 
   private uuidv4(): string {
-    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, c => {
       var r = (Math.random() * 16) | 0,
         v = c == "x" ? r : (r & 0x3) | 0x8;
       return v.toString(16);
