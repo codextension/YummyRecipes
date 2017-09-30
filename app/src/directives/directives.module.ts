ac999e4ad75f41e57aa4262a2e153f4437a56d5d
Elie Khoury <elie@el-khoury.net>
2017-09-30 20:48:19 +0200

    color fixed

--- app/src/directives/directives.module.ts
+++ app/src/directives/directives.module.ts
@@ -1,11 +1,10 @@
 import {NgModule} from "@angular/core";
-import {CardGroupDirective} from "./card-group/card-group";
 import {FocuserDirective} from "./focuser/focuser";
 
 @NgModule({
-    declarations: [CardGroupDirective, FocuserDirective],
+    declarations: [FocuserDirective],
     imports: [],
-    exports: [CardGroupDirective, FocuserDirective]
+    exports: [FocuserDirective]
 })
 export class DirectivesModule {
 }
