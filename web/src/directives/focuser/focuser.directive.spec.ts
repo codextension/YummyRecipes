ef7b4b001a6d87d22cfd6525fadb1798c78ff6e4
Elie Khoury <elie.kh@gmail.com>
2019-06-27 22:46:48 +0200

    now the old version works

--- web/src/directives/focuser/focuser.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FocuserDirective } from './focuser.directive';
-
-describe('FocuserDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FocuserDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- web/src/directives/select-text/select-text.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SelectTextDirective } from './select-text.directive';
-
-describe('SelectTextDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SelectTextDirective();
-    expect(directive).toBeTruthy();
-  });
-});
